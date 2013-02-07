4b036e17d71d2d45cfd62483a41eff6cd57154cd
Jeffrey Naval <epsilogs@gmail.com>
2013-02-07 17:39:52 +0800

    add GitPackage.py

---
+++ GitPackage.py
@@ -0,0 +1,67 @@
+import sublime, sublime_plugin, os
+
+class GitPackageCommand(sublime_plugin.WindowCommand):
+
+	s = None
+	packageList = []
+
+	defaultPath = "C:\\Program Files (x86)\\Git\\bin;C:\\Program Files\\Git\\bin"
+	packagesPlaceholder = {"Folder Name": "git://github.com/your-repository.git"}
+
+	def __init__(self, *args, **kwargs):
+		super(GitPackageCommand, self).__init__(*args, **kwargs)
+		s = sublime.load_settings('GitPackage.sublime-settings')
+		if not s.has('git_path'):
+			s.set('git_path',self.defaultPath)
+		if not s.has('packages'):
+			s.set('packages', self.packagesPlaceholder)
+		sublime.save_settings('GitPackage.sublime-settings')
+
+	def run(self, *args, **kwargs):
+		self.s = sublime.load_settings('GitPackage.sublime-settings')
+		self.list_packages()
+
+	def list_packages(self):
+		packages = self.s.get('packages')
+		self.packageList = []
+
+		if not packages:
+			self.s.set('packages', self.packagesPlaceholder)
+			sublime.save_settings('GitPackage.sublime-settings')
+			packages = self.s.get('packages')
+
+		for name, url in packages.iteritems():
+			self.packageList.append([name, url])
+
+		self.window.show_quick_panel(self.packageList,
+									 self.set_package_location)
+
+	def set_package_location(self, index):
+		if (index > -1):
+			git_url = self.packageList[index][1]
+			folder_name = self.packageList[index][0]
+			working_dir = os.path.join(sublime.packages_path(), folder_name)
+			path = self.s.get('git_path')
+			if not path:
+				self.s.set('git_path', self.defaultPath)
+				sublime.save_settings('GitPackage.sublime-settings')
+				path = self.s.get('git_path')
+
+			if not os.path.exists(working_dir):
+				os.makedirs(working_dir)
+				self.install(working_dir,path)
+
+			self.window.run_command(
+				'exec',
+				{ 'cmd':['git','pull',git_url,'master'],
+				  'working_dir':working_dir,
+				  'path': path
+				})
+
+	def install(self, working_dir, path):
+		self.window.run_command(
+			'exec',
+			{ 'cmd':['git','init'],
+			  'working_dir':working_dir,
+			  'path': path
+			})
